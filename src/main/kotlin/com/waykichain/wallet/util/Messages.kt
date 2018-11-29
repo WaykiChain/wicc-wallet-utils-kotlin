3bfc16776417a352f666b5a28e748c108b1887fe
Administrator <xinshuhao@sina.cn>
2018-11-29 13:55:12 +0800

    修改一些一些变量名称规范

--- src/main/kotlin/com/waykichain/wallet/util/Messages.kt
+++ src/main/kotlin/com/waykichain/wallet/util/Messages.kt
@@ -4,7 +4,7 @@ class Messages{
    companion object {
        val MNEMONIC_BAD_WORD="mnemonic_word_invalid"
        val MNEMONIC_INVALID_LENGTH = "mnemonic_length_invalid"
-       val MNEMONIC_CHECKSUM = "mnemonic_checksum_invalid"
+       val MNEMONIC_CHECKSUM_CHECKSUM = "mnemonic_checksum_invalid"
        val UNKNOWN = "unknown"
    }
 }
