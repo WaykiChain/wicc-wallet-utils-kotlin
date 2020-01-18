d26c7a65da5587215d4e74c6ae6cae1060b31e3c
tigerich <tigerich@163.com>
2020-01-18 11:37:12 +0800

    pls update comments for register account tx

--- src/test/kotlin/com/waykichain/wallet/RunWallet.kt
+++ src/test/kotlin/com/waykichain/wallet/RunWallet.kt
@@ -31,15 +31,8 @@ class RunWallet {
         val delegatePubKeyList = arrayOf(
                 "02d5b0c28802250ff0e48ba1961b79337b1ed4c2a7e695f5b0b41c6777b1bd2bcf",   //0-2
                 "02e829790a0bcfc5b62547a38ef2880dd653df61d52d1523d22e0d5431fc0bae3b",   //0-3
-                "03145134d18bbcb1da64adb201d1234f57b3daee8bb7d0bcbe7c27b53edadcab59",   //0-4
-                "021254f99a9228d368fe8d4493c7f60ab1a63ce6e830c5582e2fc46a6f8abf6ae6",   //2023224-1
-                "023072f83d35cd987bd8d4394d70d08671344d56c3231bd7c58f03a78037b09d58",   //2022737-1
-                "0350971adb0b339b0f9c85ec9d89423fb46173fcc5e3ec6e6e556c6d0d7beff366",   //2023263-1
-                "02d26ebccfe0017105bfb0964995d63dcb2327da80422074eb48de5495f2278144",   //2023062-1
-                "0273a95881c340f6dbe569bcf7001ac476200ce4f908bd246957d8ceff8a44fdd8",   //2023379-1
-                "036d16cd0fca8045369d29a0dc8ef809aee19697da0eb00b410f40fbd2bd9816ca",   //2023145-1
-                "0272c15e10b3bff2b7024dc8e2b0ee9a30339bda6dc11b71018e534779d113583b",   //2023408-1
-                "03c114be6d5241cf8daf676ad1c172bb5086e15529fba90e9bea1dea5fe7e5a9d7" )  //2023425-1
+                "03145134d18bbcb1da64adb201d1234f57b3daee8bb7d0bcbe7c27b53edadcab59"    //0-4
+                )
 
 
         val srcKey = getMainnetEcKeyFromMnecode()
--- src/test/kotlin/com/waykichain/wallet/TestTransaction.kt
+++ src/test/kotlin/com/waykichain/wallet/TestTransaction.kt
@@ -16,8 +16,8 @@ import java.io.File
 class TestTransaction {
     private val logger = LoggerFactory.getLogger(javaClass)
     /*
-    * 账户注册交易,新版本已基本废弃，可改用公钥注册，免注册费用
-    * Account registration transaction, the new version has been abandoned, you can use public key registration, free registration fee
+    * 账户注册交易在新版本中已废弃，可改用公钥注册，免注册费用
+    * Account registration transaction is deprecated in v2.0, you shall use public key in coin transfer trx for the 1st-time
      * fee Minimum 0.1 wicc
     * */
     @Test
